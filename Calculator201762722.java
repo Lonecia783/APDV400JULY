851330b1b7db2475060a49276121c8ce06c099ce
Lonecia <201762722@ump.ac.za>
2020-07-29 13:48:50 +0200

    Java commit git project

---
+++ Calculator201762722.java
@@ -0,0 +1,11 @@
+public class Calculator201762722{
+    public static void main(String[] args) {
+        double a = 4.5;
+        double b = 2.5;
+        
+      System.out.println("Sum is: " + (a + b));
+      System.out.println("Difference is: " + (a - b));
+      System.out.println("Product is: " + (a * b));
+      System.out.println("Quotient is: " + (a / b));
+    }
+}
